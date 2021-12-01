736ab2d3936ab5e93778212e2d68919f4deeb923
Allan Menoret <allan.menoret@gmail.com>
2021-12-01 12:10:10 +0100

    day01 part 01

--- day01/tests/Core.Tests/Tests.fs
+++ day01/tests/Core.Tests/Tests.fs
@@ -2,7 +2,17 @@ module Tests
 
 open System
 open Xunit
+open FsUnit
+
+open Inputs
+
+let Part1 depths =
+    depths |> List.pairwise |> List.map (fun (a,b) -> b-a) |> List.filter (fun x -> x > 0) |> List.length
 
 [<Fact>]
 let ``My test`` () =
-    Assert.True(true)
+    Part1 [199;200;208;210;200;207;240;269;260;263] |> should equal 7
+
+[<Fact>]
+let ``Part 01`` () =
+    Part1 measurements |> should equal 7
