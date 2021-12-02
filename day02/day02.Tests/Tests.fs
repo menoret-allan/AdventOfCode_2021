1af75ee24aaaf619723ab03244d6ff3f91dbf84b
Allan Menoret <allan.menoret@gmail.com>
2021-12-02 11:32:22 +0100

    day02 part01

--- day02/day02.Tests/Tests.fs
+++ day02/day02.Tests/Tests.fs
@@ -2,7 +2,27 @@ module Tests
 
 open System
 open Xunit
+open FsUnit
+
+open Inputs
+
+let add (x,y) (x1,y1) = (x+x1,y+y1)
+
+let parse (str: string) =
+    match str.Split ' ' with
+    | [|"forward"; x|] -> (int x, 0)
+    | [|"down"; x|] -> (0, int x)
+    | [|"up"; x|] -> (0, -(int x))
+    | _ -> (0,0)
+
+let Part1 (cmd: string) =
+    let (x, y) = (cmd.Split '\n') |> Array.map parse |> Array.reduce add
+    x*y
 
 [<Fact>]
-let ``My test`` () =
-    Assert.True(true)
+let ``Part 1 small test`` () =
+    commandsSmall |> Part1 |> should equal 150
+
+[<Fact>]
+let ``Part 1 test`` () =
+    commandsBig |> Part1 |> should equal 1962940
